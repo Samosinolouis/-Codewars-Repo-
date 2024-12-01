7ee289b8bc57907512463c9bd1e6179b091b2b33
Samosinolouis <Samosinolouis09@gmail.com>
2024-12-01 19:14:23 +0800

    Stop gninnipS My sdroW.py

---
+++ 6kyu/Stop gninnipS My sdroW.py
@@ -0,0 +1,14 @@
+def spin_words(sentence):
+    words = sentence.split()
+    reversed_words = []
+    
+    for word in words:
+        if len(word) >= 5:
+            reversed_words.append(word[::-1])
+        else:
+            reversed_words.append(word)
+    return " ".join(reversed_words)
+
+sentence = "Hey fellow warriors"
+result = spin_words(sentence)
+print(result)
